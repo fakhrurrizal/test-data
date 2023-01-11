a183c54394c60db866a1ac6a11a353a0787651cc
Muhammad Fakhrur Rizal <fakhrurrizal10@gmail.com>
2023-01-11 17:00:45 +0700

    deploy test

--- server/main.go
+++ server/main.go
@@ -9,14 +9,15 @@ import (
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
-	// errEnv := godotenv.Load()
-	// if errEnv != nil {
-	// 	panic("Failed to load env file")
-	// }
+	errEnv := godotenv.Load()
+	if errEnv != nil {
+		panic("Failed to load env file")
+	}
 
 	mysql.DatabaseInit()
 
